f9d174c979309439a5fa65b317dd230a2fa492d1
Livio Ribeiro <liviomreboucas@gmail.com>
2024-06-04 14:03:51 -0300

    fazendo alteracoes e adicionando logs na server.js e index.js/migrations

--- src/database/index.js
+++ src/database/index.js
@@ -1,29 +1,61 @@
-import Sequelize from 'sequelize'
-import configDatabase from '../config/database.js'
-import User from '../app/models/User'
-import Products from '../app/models/Product.js'
-import Category from '../app/models/Category.js'
-import mongoose from 'mongoose'
-const models = [User, Products, Category]
+import Sequelize from 'sequelize';
+import User from '../app/models/User.js';
+import Products from '../app/models/Product.js';
+import Category from '../app/models/Category.js';
+import mongoose from 'mongoose';
+
+const models = [User, Products, Category];
+
 class Database {
   constructor() {
-    this.init()
-    this.mongo()
+    this.init();
+    this.mongo();
   }
 
   init() {
-    this.connection = new Sequelize('postgresql://postgres:zaBvVrtRDTnOMUnyCxJjoLcCQmNBdIsj@roundhouse.proxy.rlwy.net:23029/railway')
+    this.connection = new Sequelize(process.env.DATABASE_URL || 'postgresql://postgres:zaBvVrtRDTnOMUnyCxJjoLcCQmNBdIsj@roundhouse.proxy.rlwy.net:23029/railway', {
+      dialect: 'postgres',
+      protocol: 'postgres',
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false
+        }
+      },
+      logging: console.log // Adicionado para logs de Sequelize
+    });
+
+    this.connection.authenticate()
+      .then(() => {
+        console.log('Connection to PostgreSQL has been established successfully.');
+      })
+      .catch(err => {
+        console.error('Unable to connect to PostgreSQL:', err);
+      });
+
     models
       .map((model) => model.init(this.connection))
-      .map(
-        (model) => model.associate && model.associate(this.connection.models),
-      )
+      .map((model) => model.associate && model.associate(this.connection.models));
   }
 
   mongo() {
-    this.mongoCOnnection = mongoose.connect(
-      'mongodb://mongo:BtOUesdJjTyIpPCqXPiuvmTmvMsyVuuR@viaduct.proxy.rlwy.net:46264',
-    )
+    const mongoURI = process.env.MONGO_URL || 'mongodb://mongo:BtOUesdJjTyIpPCqXPiuvmTmvMsyVuuR@viaduct.proxy.rlwy.net:46264';
+
+    mongoose.connect(mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+    .then(() => {
+      console.log('Connected to MongoDB');
+    })
+    .catch((error) => {
+      console.error('Error connecting to MongoDB', error);
+    });
+
+    mongoose.connection.on('error', err => {
+      console.error('MongoDB connection error:', err);
+    });
   }
 }
-export default new Database()
+
+export default new Database();
