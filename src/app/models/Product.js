1e8f9169a836f6a1925d34c234e7c461b36c74e3
Livio Ribeiro <liviomreboucas@gmail.com>
2024-06-12 20:26:22 -0300

    alterando models de product.js - adicionando params do railway

--- src/app/models/Product.js
+++ src/app/models/Product.js
@@ -12,7 +12,7 @@ class Products extends Model {
         url: {
           type: Sequelize.VIRTUAL,
           get() {
-            return `http://localhost:3001/product-file/${this.path}`
+            return `https://codeburger-production-f8a0.up.railway.app/product-file/${this.path}`;
           },
         },
       },
