05701d92763ea66140a8b45e3818f231ba6b7d89
Livio Ribeiro <liviomreboucas@gmail.com>
2024-06-12 19:44:06 -0300

    tentativa de incrementacao 3 na app.js - ajustando cors

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ class App {
     
     // Middleware para permitir CORS para um único domínio
     this.app.use((req, res, next) => {
-      res.setHeader('Access-Control-Allow-Origin', 'http://code-burger-frontend-livio-dev.vercel.app')
+      res.setHeader('Access-Control-Allow-Origin', 'https://code-burger-frontend-livio-dev.vercel.app/')
       res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
       res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
       res.setHeader('Access-Control-Allow-Credentials', true)
