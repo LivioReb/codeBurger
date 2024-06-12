8f09fdff31bec9ff5e45923f555714e211347a43
Livio Ribeiro <liviomreboucas@gmail.com>
2024-06-12 19:54:20 -0300

    tetativa 7 alteracao de cors

--- src/app.js
+++ src/app.js
@@ -1,41 +1,43 @@
-import express from 'express'
-import routes from './routes.js'
-import { resolve } from 'path'
-import cors from 'cors'
-import './database'
-
-
+import express from 'express';
+import routes from './routes.js';
+import { resolve } from 'path';
+import cors from 'cors';
+import './database';
 
 class App {
-
-  
   constructor() {
-    const corsOptions ={
-      origin: 'https://code-burger-frontend-qsdo.vercel.app/', 
-      credentials: true,
-    }
-    this.app = express()
-    this.app.use(cors(corsOptions))
+    this.app = express();
 
-    this.middlewares()
-    this.routes()
+    const corsOptions = {
+      origin: 'https://code-burger-frontend-qsdo.vercel.app',
+      credentials: true,
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      allowedHeaders: ['Content-Type', 'Authorization'],
+    };
+
+    // Aplica as opções de CORS
+    this.app.use(cors(corsOptions));
+
+    this.middlewares();
+    this.routes();
   }
 
   middlewares() {
-    this.app.use(express.json())
+    this.app.use(express.json());
     this.app.use(
       '/product-file',
       express.static(resolve(__dirname, '..', 'uploads')),
-    )
+    );
 
     this.app.use(
       '/category-file',
       express.static(resolve(__dirname, '..', 'uploads')),
-    )
+    );
   }
 
   routes() {
-    this.app.use(routes)
+    this.app.use(routes);
   }
 }
-export default new App().app
+
+export default new App().app;
